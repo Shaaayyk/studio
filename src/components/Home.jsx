6f22c41ad98922b030bac5020032f0f65837d5d0
Shay Kelley <shay@Shays-MacBook.local>
2019-10-22 15:51:14 -0400

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import background2 from '../images/background2.jpg'
+import gif from '../images/gif.gif'
 
 export default function Home(props) {
   return (
     <div id='home'>
-      <img src={background2} alt='background' className='background' />
+      <img src={gif} alt='background' className='background' />
       <h1>STUDIO GHIBLI</h1>
       <p>Studio Ghibli, Inc. is a Japanese animation film studio based in Koganei, Tokyo, Japan. The studio is best known for its anime feature films, and has also produced several short films, television commercials, and one television film. </p>
       <Link to='/films'>View All Films</Link>
-
     </div>
   )
 }
